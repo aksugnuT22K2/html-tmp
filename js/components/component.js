afebdaa4d208ac2f23905bb47ce98da41c6da6d1
test <test@test.test>
2018-08-29 03:03:18 +0900

    fix

--- js/components/component.js
+++ js/components/component.js
@@ -6,8 +6,8 @@ Vue.component('head-menu-items', {
         <i class="material-icons" v-if="menuItem.icon !== ''">{{ menuItem.icon }}</i>
         {{ menuItem.text }}
       </a>
-    </li>`
-});
+    </li>`,
+})
 
 Vue.component('drawer-btn', {
   props: ['drawer'],
@@ -17,5 +17,18 @@ Vue.component('drawer-btn', {
         <label :for="drawer.id" id="open" class="btn-circle" :class="{checked: drawer.isShow}">
           <i class="material-icons">{{ drawer.icon }}</i>
         </label>
-    </div>`
-});
+    </div>`,
+})
+
+Vue.component('m-table', {
+  props: {tableData: Object, max: {type: Number, default: 100}},
+  template: `
+    <table class="table" border="1">
+      <tr>
+          <th v-for="(item, key) in tableData.th" v-if="key < max" :key="key" :title="item">{{ item }}</th>
+      </tr>
+      <tr v-for="(tr, key) in tableData.tr">
+          <td v-for="(item, key, index) in tr" v-if="key < max" :title="item"> {{ item }}</td>
+      </tr>
+  </table>`
+})
--- js/index.js
+++ js/index.js
@@ -4,13 +4,23 @@ let app = new Vue({
   data: {
     //drawer
     drawers: {
-      right: { id: 'right-drawer', isShow: false, icon: 'menu' },
+      right: { id: 'right-drawer', isShow: false, icon: 'more_vert' },
       left: { id: 'left-drawer', isShow: false, icon: 'menu' }
     },
     //head-menu-item
     HeadMenuItems: [
-      { text: 'item1', url: '#', icon: '' },
-      { text: 'item2', url: '#', icon: '' }
-    ]
+      { text: 'item1', url: '#', icon: 'apps' },
+      { text: 'item2', url: '#', icon: 'settings' }
+    ],
+    //ad-table
+    tableData: {
+      th: [
+        'tr1', 'tr2', 'tr3', 'tr4', 'tr5', 'tr6', 'tr7', 'tr8', 'tr9'
+      ],
+      tr: [
+        ['td1', 'td2', 'td3', 'td4', 'td5', 'td6', 'td7', 'td8', 'td9'],
+        ['td1', 'td2', 'td3', 'td4', 'td5', 'td6', 'td7', 'td8', 'td9']
+      ]
+    },
   }
 });
